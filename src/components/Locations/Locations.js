23f74de92b6637fa9eeadc62a7bc7a67658e93be
Evan Rosen <Evan@admins-MacBook-Pro.local>
2019-09-11 12:41:07 -0700

    added styling on Locations page

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Segment, Container, Header } from "semantic-ui-react";
 import LocationList from "./LocationList";
 import { TopNav } from "../layout/TopNav";
 import { Footer } from "../layout/Footer";
+import Mazda from "../Homepage/mazda.jpg";
 
 const Locations = props => {
   return (
     <div>
-      <TopNav></TopNav>
-      <h1>Locations</h1>
-      <button onClick={props.history.goBack}>Back</button>
-      <LocationList></LocationList>
+      <Segment
+        inverted
+        textAlign="center"
+        style={{
+          minHeight: 700,
+          padding: "1em 0em",
+          backgroundImage: `url(${Mazda})`
+        }}
+        vertical
+      >
+        <TopNav></TopNav>
+        <Container style={{ backgroundColor: "white" }}>
+          <Header as="h1">Locations</Header>
+          <button onClick={props.history.goBack}>Back</button>
+          <LocationList></LocationList>
+        </Container>
+      </Segment>
       <Footer />
     </div>
   );
